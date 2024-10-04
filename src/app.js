73fc3b7797fc78afec170c954f2cf6757213f50b
Josef <jm224ae@student.lnu.se>
2024-10-04 09:03:57 +0200

    W2

---
+++ src/app.js
@@ -0,0 +1,9 @@
+/**
+ * workshop 2
+ */
+
+// Hämta elementet med id "message"
+const messageElement = document.getElementById('message');
+
+// Ändra textinnehållet i elementet
+messageElement.textContent = 'Hello from JavaScript module!';
